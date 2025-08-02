91cfda34072bde7061521589570c48bf2ab38c5c
Gleb Voronov <glebvoronov309@gmail.com>
2025-08-02 17:18:25 +0300

    fixed files

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,25 +1,50 @@
-import Contact from '../models/contact.js';
+import * as contactsService from '../services/contacts.js';
 
 export const getAllContacts = async (req, res) => {
   try {
-    const contacts = await Contact.find();
-    res.json(contacts);
+    const contacts = await contactsService.getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!' ,
+      data: {
+        contacts,
+      },
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      status: 'error',
+      code: 500,
+      message: error.message,
+    });
   }
 };
 
 export const getContactById = async (req, res) => {
   try {
     const { id } = req.params;
-    const contact = await Contact.findById(id);
+    const contact = await contactsService.getContactById(id);
 
     if (!contact) {
-      return res.status(404).json({ message: 'Contact not found' });
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'Not found',
+      });
     }
 
-    res.json(contact);
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${id}!`,
+      data: {
+        contact,
+      },
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      status: 'error',
+      code: 500,
+      message: error.message,
+    });
   }
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,9 @@
 import Contact from '../models/contact.js';
 
 export const getAllContacts = async () => {
-  return await Contact.find();
+  return await Contact.find({}, '-__v -createdAt -updatedAt');
 };
 
 export const getContactById = async (id) => {
-  return await Contact.findById(id);
+  return await Contact.findById(id, '-__v -createdAt -updatedAt');
 };
