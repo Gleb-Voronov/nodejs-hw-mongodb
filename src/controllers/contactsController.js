fc23917a73704cc13b03e9c3d9994f50c36c74d9
Gleb Voronov <glebvoronov309@gmail.com>
2025-08-03 14:39:43 +0300

    fixed file contact

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -6,10 +6,8 @@ export const getAllContacts = async (req, res) => {
 
     res.status(200).json({
       status: 200,
-      message: 'Successfully found contacts!' ,
-      data: {
-        contacts,
-      },
+      message: 'Successfully found contacts!',
+      data: contacts, 
     });
   } catch (error) {
     res.status(500).json({
@@ -36,9 +34,7 @@ export const getContactById = async (req, res) => {
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${id}!`,
-      data: {
-        contact,
-      },
+      data: contact, 
     });
   } catch (error) {
     res.status(500).json({
